95dfefb41f7224d33724610965526ac367dab233
Imir0nMan <104762588+Imir0nMan@users.noreply.github.com>
2022-12-16 19:02:31 +0400

    Update new.py

--- new.py
+++ new.py
@@ -21,7 +21,7 @@ class AltKey():
         self.pressType = presstype
         self.delay=delay
 
-keys = [Key("start chrome /incognito facebook.com",1),AltKey("enter",0,0.1),Key("ashot_gasparyan_2002@mail.ru",2),AltKey("tab",0,0.1),Key("mam1975pap1977",0.1),AltKey("enter",0,0.1),AltKey("enter",0,2),AltKey("tab",10,5),AltKey("enter",0,0.2),AltKey("tab",4,0),AltKey("enter",0,0.5),Key("Ashot Gasparyan",1)]
+keys = [Key("start chrome /incognito facebook.com",1),AltKey("enter",0,0.1),Key("sample",2),AltKey("tab",0,0.1),Key("sample",0.1),AltKey("enter",0,0.1),AltKey("enter",0,2),AltKey("tab",10,5),AltKey("enter",0,0.2),AltKey("tab",4,0),AltKey("enter",0,0.5),Key("Ashot Gasparyan",1)]
 
 def main():
     for key in keys:
